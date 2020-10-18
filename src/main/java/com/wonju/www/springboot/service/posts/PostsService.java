9093c4b26937e4271c06b9467d1ebaf7596f1314
wonjuyang <his1monologue@gmail.com>
2020-10-18 22:58:07 +0900

    insert added

---
+++ src/main/java/com/wonju/www/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.wonju.www.springboot.service.posts;
+
+
+import com.wonju.www.springboot.domain.posts.PostsRepository;
+import com.wonju.www.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/wonju/www/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,4 @@
+package com.wonju.www.springboot.web.dto;
+
+public class PostsResponseDto {
+}
