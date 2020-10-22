360b230744c5f117d058cffa861589b23a5c87aa
wonjuyang <his1monologue@gmail.com>
2020-10-22 23:56:29 +0900

    findAll added

--- src/main/java/com/wonju/www/springboot/service/posts/PostsService.java
+++ src/main/java/com/wonju/www/springboot/service/posts/PostsService.java
@@ -3,13 +3,17 @@ package com.wonju.www.springboot.service.posts;
 
 import com.wonju.www.springboot.domain.posts.Posts;
 import com.wonju.www.springboot.domain.posts.PostsRepository;
+import com.wonju.www.springboot.web.dto.PostsListResponseDto;
 import com.wonju.www.springboot.web.dto.PostsResponseDto;
 import com.wonju.www.springboot.web.dto.PostsSaveRequestDto;
 import com.wonju.www.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -30,9 +34,17 @@ public class PostsService {
         return id;
     }
 
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+
 }
--- src/main/java/com/wonju/www/springboot/web/IndexController.java
+++ src/main/java/com/wonju/www/springboot/web/IndexController.java
@@ -1,15 +1,26 @@
 package com.wonju.www.springboot.web;
 
 
+import com.wonju.www.springboot.domain.posts.Posts;
+import com.wonju.www.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){ // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
+
 
     @GetMapping("/posts/save")
     public String postsSave(){
