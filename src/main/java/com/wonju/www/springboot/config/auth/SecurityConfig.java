409c560bee0af12bf5a4be5343a40a18d52bfc73
sowp <sowp_2020@naver.com>
2020-11-03 23:58:58 +0900

    security edited

--- src/main/java/com/wonju/www/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/wonju/www/springboot/config/auth/SecurityConfig.java
@@ -4,4 +4,7 @@ package com.wonju.www.springboot.config.auth;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+
 }
